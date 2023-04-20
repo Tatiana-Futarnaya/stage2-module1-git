1db6b6e7493b786640698bb4f4b6816141e3af71
Tatiana-Futarnaya <tannia93@mail.ru>
2023-04-20 14:17:17 +0300

    third commit

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Tatiana-Futarnaya/gitTask.git";
+    public static String REPO_LINK = "https://github.com/Tatiana-Futarnaya/gitCode.git";
 }
